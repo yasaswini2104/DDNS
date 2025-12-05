6804455399613f58e9a4052b756243c939b946dc
yasaswini2104 <yasaswini.gaddam21@gmail.com>
2025-12-05 17:55:22 +0530

    refactor: move wallet connection logic from App.jsx into WalletBar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,40 +4,17 @@ import SearchDomain from "./pages/SearchDomain";
 import ResolveDomain from "./pages/ResolveDomain";
 import UpdateCID from "./pages/UpdateCID";
 import UploadPage from "./pages/UploadPage";
+import WalletBar from "./components/WalletBar";
+
 
 function App() {
-  const [account, setAccount] = useState(null);
-
-  async function connectWallet() {
-    if (!window.ethereum) {
-      alert("MetaMask not installed!");
-      return;
-    }
-
-    try {
-      const accounts = await window.ethereum.request({
-        method: "eth_requestAccounts",
-      });
-
-      setAccount(accounts[0]);
-      console.log("Connected:", accounts[0]);
-    } catch (err) {
-      console.error("Wallet connection failed:", err);
-    }
-  }
 
   return (
     <div className="container">
       <h1>Decentralized DNS (DDNS)</h1>
 
-      {/* Connect Wallet Button */}
-      <div style={{ marginBottom: "20px" }}>
-        {account ? (
-          <p><strong>Connected Wallet:</strong> {account}</p>
-        ) : (
-          <button onClick={connectWallet}>Connect Wallet</button>
-        )}
-      </div>
+      
+      <WalletBar />
 
       <UploadPage />
 
